06636c8b5883fa861b4b6fa54c6ea4993bc1c859
lindavanvelzen <lindavanvelzen@valori.nl>
2018-09-18 14:51:27 +0200

    added assignments

---
+++ src/test/java/chapterSix/FillCartTest.java
@@ -0,0 +1,60 @@
+package chapterSix;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.assertj.core.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class FillCartTest {
+
+    @Test
+    public void emptyCart() throws InterruptedException {
+        ChromeDriverManager.getInstance().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        driver.get("http://automationpractice.com/");
+        driver.manage().window().maximize() ;
+
+        //Cart empty
+        Assertions.assertThat(driver.findElement(By.className("ajax_cart_no_product")).isDisplayed()).isTrue();
+
+        //Search dress
+        driver.findElement(By.id("search_query_top")).sendKeys("dress");
+        driver.findElement(By.cssSelector(".button-search")).click();
+        driver.findElement(By.xpath("//*[@class='product_list grid row']//img[@title='Printed Chiffon Dress']")).click();
+
+
+        driver.switchTo().defaultContent(); // you are now outside both frames
+
+        //Switch iframe & add to cart
+        driver.switchTo().frame(driver.findElement(By.className("fancybox-iframe")));
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("#add_to_cart"))));
+        driver.findElement(By.cssSelector("#add_to_cart")).click();
+
+
+        driver.switchTo().parentFrame();
+
+        //Confirmation cart
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("layer_cart_overlay"))));
+        Assertions.assertThat(driver.findElement(By.className("icon-ok")).isDisplayed()).isTrue();
+
+        //Continue shopping
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//*[@id='header']/div[3]/div/div/div[3]/div/a/span[1]"))));
+        driver.findElement(By.xpath("//*[@id='layer_cart']/div[1]/div[2]/div[4]/span")).click();
+
+
+        Assertions.assertThat(driver.findElement(By.xpath("//div[@class='shopping_cart']//span[@class='ajax_cart_quantity']")).getText()).isEqualTo("1");
+ 
+
+        driver.quit();
+    }
+
+
+
+}
--- src/test/java/chapterSix/SignOutTest.java
+++ src/test/java/chapterSix/SignOutTest.java
@@ -1,37 +1,37 @@
-package examples;
+package chapterSix;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-
-
-public class FirstSeleniumTest {
-
+public class SignOutTest {
 
     @Test
     public void logInSuccesfull() {
         ChromeDriverManager.getInstance().setup();
         WebDriver driver = new ChromeDriver();
 
+
+
         driver.get("http://automationpractice.com/");
+
+
+        driver.manage().window().maximize();
+
         driver.findElement(By.className("login")).click();
 
         driver.findElement(By.id("email")).sendKeys("dantester@jourrapide.com");
         driver.findElement(By.id("passwd")).sendKeys("1qazxsw2");
         driver.findElement(By.id("SubmitLogin")).click();
+        Assertions.assertThat(driver.getTitle()).as("Login failed").isEqualToIgnoringCase("My account - My store");
 
-
-        Assertions.assertThat(driver.getTitle()).as("Not on the right page").isEqualToIgnoringCase("My account - My store");
+        driver.findElement(By.className("logout")).click();
+        Assertions.assertThat(driver.findElement(By.className("login")).getText()).as("Logout failed").isEqualTo("Sign in");
 
         driver.quit();
     }
-
-
 
 }
